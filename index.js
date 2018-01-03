bc6b39d9dd89272316c1cc7cb7aec39277c5b0b7
da crands <davecrands@gmail.com>
2018-01-02 21:41:01 -0500

    Added typing function. An experience in queueing funcs. Meat of code from w3schools

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
+// Typing effect
+var automation =  "Automation";
+document.getElementById("automation");
+
+console.log(automation);
+
+// function typer() {
+//   for(i=0; i<automation.length; i++){
+//     document.getElementById("automation").innerHTML += automation[i];
+//     setTimeout(typer, 500);
+//   }
+// }
+
+// Taken from w3schools
+// I don't feel good about what I am about to do
+// But until a better solution
+var i = 0;
+var txt = 'Automation';
+var speed = 150;
+var done = false;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("automation").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    console.log(i);
+  }
+  if (i === 10) {
+    typeWriter2();
+  }
+
+}
+
+var i2 = 0;
+var txt2 = 'Data Stuffs';
+speed2 = 250;
+
+function typeWriter2() {
+  if (i2 < txt2.length) {
+    document.getElementById("data").innerHTML += txt2.charAt(i2);
+    i2++;
+    setTimeout(typeWriter2, speed2);
+    if (i2 === txt2.length){
+      typeWriter3();
+    }
+  }
+}
+var i3 = 0;
+var txt3 = 'Web Dev';
+speed3 = 150;
+
+function typeWriter3() {
+  if (i3 < txt3.length) {
+    document.getElementById("web").innerHTML += txt3.charAt(i3);
+    i3++;
+    setTimeout(typeWriter3, speed3);
+  }
+
+}
+
+typeWriter();
+
+// Button
 $(".python-button").click(function(){
     var aboutHeight = $(".about").offset().top - 50;
     $("body, html").animate({
