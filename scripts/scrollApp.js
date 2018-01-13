2626d888b4e12486aa6ef04eb4b2285767645687
da crands <davecrands@gmail.com>
2018-01-13 16:22:03 -0500

    Better scroll timing

--- scripts/scrollApp.js
+++ scripts/scrollApp.js
@@ -1,6 +1,6 @@
 window.sr = ScrollReveal({reset: false});
 
-sr.reveal('.about-text', {duration:2000});
-sr.reveal('.frameworks', {duration:3000});
-sr.reveal('.git-table', {duration:3000});
+sr.reveal('.about-text', {duration:1000});
+sr.reveal('.frameworks', {duration:2000});
+sr.reveal('.git-table', {duration:2000});
 console.log("scroller");
