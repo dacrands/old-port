47641711a138d3b7c6f58a1d867ba1490c42cc32
da crands <davecrands@gmail.com>
2018-01-13 14:19:24 -0500

    Improved element spacing

--- scripts/scrollApp.js
+++ scripts/scrollApp.js
@@ -2,5 +2,5 @@ window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.about-text', {duration:2000});
 sr.reveal('.frameworks', {duration:3000});
-sr.reveal('.github', {duration:3000});
+sr.reveal('.git-table', {duration:3000});
 console.log("scroller");
